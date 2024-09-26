693c0b645c26574e3fbd7d42f272cac5f17513b7
Gratitude Lee <125866096+SibusisoMokoena@users.noreply.github.com>
2024-09-26 23:49:37 +0200

    Update IGenericRepository.cs

--- TelemetryPortal_MVC/Repository/IGenericRepository.cs
+++ TelemetryPortal_MVC/Repository/IGenericRepository.cs
@@ -8,15 +8,18 @@ namespace TelemetryPortal_MVC.Repository
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<bool> ExistsAsync(int id)
-        Task<T> GetByIdAsync(int id);
+        Task<bool> ExistsAsync(Guid? id)
+        Task<T> GetByIdAsync(Guid? id);
         Task AddAsync(T entity);
         void Update(T entity);
         void Remove(T entity);
 
         // custome code that were not provided:
 
-        
-
+        void save();
+        void create(T t);
+        bool Edit(Guid id, T t);
+        T checkDetails(Guid? id);
+        bool CheckId(Guid? id);
     }
 }
